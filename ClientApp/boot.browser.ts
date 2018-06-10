d2201219a08ac6491f3559eb5e2c08c1c7192327
Arne Ubelhor <aubelhor@appriss.com>
2018-06-10 19:04:55 +0100

    removed semantic for bulma

--- ClientApp/boot.browser.ts
+++ ClientApp/boot.browser.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'zone.js';
-import 'bootstrap';
+import 'bulma/css/bulma.css';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.browser.module';
